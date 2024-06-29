2e409d9cdf460a857da49e840f3db735bf1713f4
olgalambova <lambova25@gmail.com>
2024-06-29 18:38:01 +0300

    dz5.11

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-
 import string
-my_user_name = input("Enter your username: " " ")
-print(my_user_name[0].startswith('_'),
-      my_user_name[0].isdigit(),
-      my_user_name[1:].isalnum(),
-      my_user_name[0].islower(),
-      my_user_name[:].islower(),
-      my_user_name[1:].isalpha())
+my_user_name = input("Enter your username: ")
+print(my_user_name[0])
+for i in my_user_name[0]:
+      if i in list(my_user_name[0]):
+            print(my_user_name[0].isalpha())
+      if i in list(my_user_name[0]):
+            print(my_user_name[0].islower())
+      if i in list(my_user_name[0]):
+            print(my_user_name[0].isupper())
+      if i in list(my_user_name[0]):
+            print(my_user_name[0].isdigit())
+      if i in list(my_user_name[0]):
+            print(my_user_name[0].isnumeric())
+
+
+
+
+
+#print(my_user_name[0].isalpha())
+# print(my_user_name[0].islower())
+# print(my_user_name[0].isupper())
+# print(my_user_name[0].isdigit())
+# print(my_user_name[0].isnumeric())
+
+
+
+
+
+
+
 
 
 
